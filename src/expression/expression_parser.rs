9d6b947002e53d4261b2fc675da702198f709d31
Victor Nivet <victor@saumon.ninja>
2017-07-01 12:16:47 +0200

    Added optional strings and escaping with single quotes

--- src/expression/expression_parser.rs
+++ src/expression/expression_parser.rs
@@ -11,7 +11,35 @@ pub enum ParseError {
     Unknown,
 }
 
-fn make_item(string: &str) -> Result<super::Item, ParseError> {
+fn make_escaped_text_item(string: &str) -> Result<super::Item, ParseError> {
+    Ok(super::Item::Text(string.to_owned()))
+}
+
+named!(escaped_text<&[u8], super::Item, ParseError>,
+    add_return_error!(
+        ErrorKind::Custom(ParseError::NomError),
+        alt!(
+            // special rule: '' => turns to a single ' text
+            value!(
+                super::Item::Text("'".to_owned()),
+                tag!("''")
+                ) |
+            // otherwise text enclosed with single quotes ' turn into normal text
+            map_res!(
+                map_res!(
+                    delimited!(
+                        tag!("'"),
+                        take_until!("'"),
+                        tag!("'")),
+                    str::from_utf8
+                ),
+                make_escaped_text_item
+            )
+        )
+    )
+);
+
+fn make_tag_item(string: &str) -> Result<super::Item, ParseError> {
     Ok(super::Item::Tag(string.to_owned()))
 }
 
@@ -20,11 +48,51 @@ named!(item_tag<&[u8], super::Item, ParseError>,
         ErrorKind::Custom(ParseError::NomError),
         map_res!(
         delimited!(
-            char!('%'),
+            tag!("%"),
             map_res!(alphanumeric, str::from_utf8),
-            char!('%')),
-        make_item
+            tag!("%")),
+        make_tag_item
         )
+    )
+);
+
+fn make_expression_box(expression: super::Expression) -> Result<Box<super::Expression>, ParseError> {
+    Ok(Box::new(expression))
+}
+
+named!(optional_expression<&[u8], Box<super::Expression>>,
+    map_res!(
+        add_return_error!(
+            ErrorKind::Custom(42),
+            parse_expression
+            ),
+        make_expression_box
+    )
+);
+
+fn make_optional_item(expression: Box<super::Expression>) -> Result<super::Item, ParseError> {
+    Ok(super::Item::OptionalExpr(expression))
+}
+
+named!(item_optional<&[u8], super::Item, ParseError>,
+    add_return_error!(
+        ErrorKind::Custom(ParseError::NomError),
+        map_res!(
+            do_parse!(
+                tag!("[") >>
+                expr: optional_expression >>
+                tag!("]") >>
+                (expr)),
+            make_optional_item
+        )
+    )
+);
+
+named!(parse_item<&[u8], super::Item, ParseError>,
+    alt!(
+        escaped_text |
+        item_tag |
+        item_optional
     )
 );
 
@@ -44,7 +112,7 @@ fn flush_text(current_text: &mut Vec<u8>, items: &mut Vec<super::Item>) -> Resul
 
 macro_rules! flush_text {
     ($x:expr, $y:expr) => {
-        if let Err(err) = flush_text(&mut $x, &mut $y) {
+        if let Err(err) = flush_text($x, $y) {
             return IResult::Error(ErrorKind::Custom(err));
         };
     }
@@ -54,17 +122,23 @@ pub fn parse_expression(mut input: &[u8]) -> IResult<&[u8], super::Expression, P
     let mut items: Vec<super::Item> = Vec::new();
     let mut current_text: Vec<u8> = Vec::new();
     while input.len() > 0 {
-        let parse_result = item_tag(input);
-        match parse_result {
-            IResult::Done(input_remaining, new_item) => {
-                input = input_remaining;
-                flush_text!(&mut current_text, &mut items);
-                items.push(new_item);
-            },
+        // special characters
+        match input[0] {
+            b']' => break,
             _ => {
-                current_text.push(input[0]);
-                input = &input[1..];
-            },
+                let parse_result = parse_item(input);
+                match parse_result {
+                    IResult::Done(input_remaining, new_item) => {
+                        input = input_remaining;
+                        flush_text!(&mut current_text, &mut items);
+                        items.push(new_item);
+                    },
+                    _ => {
+                        current_text.push(input[0]);
+                        input = &input[1..];
+                    },
+                }
+            }
         }
     }
     flush_text!(&mut current_text, &mut items);
@@ -74,7 +148,6 @@ pub fn parse_expression(mut input: &[u8]) -> IResult<&[u8], super::Expression, P
     IResult::Done(input, expression)
 }
 
-
 pub fn parse(string: &str) -> Result<super::Expression, ParseError> {
     let result = parse_expression(string.as_bytes());
     match result {
@@ -83,16 +156,3 @@ pub fn parse(string: &str) -> Result<super::Expression, ParseError> {
         _ => Err(ParseError::Unknown),
     }
 }
-/*
-#[test]
-fn parse_simple_text() {
-  let simple_text_string = &b"simple text string"[..];
-
-  let res = parse(simple_text_string);
-  println!("{:?}", res);
-
-  let test = super::Expression {
-      items: vec![super::Item::Text("simple text string"),]
-  };
-  assert_eq!(res, test);
-}*/
--- src/expression/mod.rs
+++ src/expression/mod.rs
@@ -4,7 +4,7 @@ use super::metadata::MetadataObject;
 pub enum Item {
 	Text(String),
 	Tag(String),
-    OptionalExpression(Box<Expression>),
+    OptionalExpr(Box<Expression>),
 }
 
 pub struct Expression {
@@ -17,20 +17,34 @@ impl Expression {
     pub fn parse(string: &str) -> Result<Expression, expression_parser::ParseError> {
         Ok(expression_parser::parse(string)?)
     }
-	pub fn apply(&self, metadata: &MetadataObject) -> String {
+	fn apply_optional(&self, metadata: &MetadataObject) -> (String, u32) {
 		let mut s = String::new();
+		let mut tags_found : u32 = 0; 
 		for item in self.items.iter() {
 			match item {
 				&Item::Text(ref text) => s.push_str(text),
 				&Item::Tag(ref text) => {
 					let result = metadata.read_tag(text.to_lowercase().as_str());
 					if let Some(result_string) = result {
-						s.push_str(result_string.as_str())
+						s.push_str(result_string.as_str());
+						if result_string.len() > 0 {
+							tags_found += 1;
+						}
 					}
 				},
-				&Item::OptionalExpression(_) => (),
+				&Item::OptionalExpr(ref expr) => {
+					let (expr_s, expr_tag) = expr.apply_optional(metadata);
+					if expr_tag > 0 {
+						s.push_str(expr_s.as_str());
+						tags_found += expr_tag;
+					}
+				},
 			}
 		}
+		(s, tags_found)
+	}
+	pub fn apply(&self, metadata: &MetadataObject) -> String {
+		let (s, _) = self.apply_optional(metadata);
 		s
 	}
     pub fn definition(&self) -> String {
@@ -43,7 +57,11 @@ impl Expression {
 						s.push_str(text.as_str());
 						s.push_str("%");
 					},
-				&Item::OptionalExpression(_) => (),
+				&Item::OptionalExpr(ref expr) => {
+					s.push_str("[");
+					s.push_str(expr.definition().as_str());
+					s.push_str("]");
+				},
 			}
 		}
         s
