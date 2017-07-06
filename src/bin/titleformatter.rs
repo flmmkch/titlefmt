c3a3450d1fedc78ded02a59ad924fc20c1a2b23d
Victor Nivet <victor@saumon.ninja>
2017-07-06 22:45:27 +0200

    Added usage guide for the titleformatter utility and better error handling

--- src/bin/titleformatter.rs
+++ src/bin/titleformatter.rs
@@ -11,6 +11,7 @@ fn main() {
 	let args: Vec<String> = env::args().collect();
 	let formatter = Formatter::new();
 	let parser = formatter.parser();
+	let mut display_help = true;
 	if args.len() > 2 {
 		// first argument: expression
 		let expression_string = String::from(args[1].to_owned());
@@ -23,17 +24,27 @@ fn main() {
 			filenames_builder
 		};
 		// parse the expression
-		let expression = parser.parse(expression_string.as_str()).unwrap();
-		// read the various files and apply it to them
-		for filename in filenames.iter() {
-			let audio_file_result = AudioFile::read_file(filename);
-			match audio_file_result {
-				Ok(audio_file) => {
-					let line = expression.apply(&audio_file);
-					println!("{}", line);
-				},
-				Err(_) => continue,
-			};
+		match parser.parse(expression_string.as_str()) {
+			Ok(expression) => {
+				display_help = false;
+				// read the various files and apply it to them
+				for filename in filenames.iter() {
+					let audio_file_result = AudioFile::read_file(filename);
+					match audio_file_result {
+						Ok(audio_file) => {
+							let line = expression.apply(&audio_file);
+							println!("{}", line);
+						},
+						Err(_) => continue,
+					};
+				}
+			}
+			Err(error) => println!("Invalid expression: {:?}", error),
 		}
+	}
+	if display_help {
+		println!("Usage:");
+		println!("titleformatter [expression] [filenames...]");
+		println!("where [expression] is a Title Formatting expression, such as \"%tracknumber%. [%artist% - ]%title%\"");
 	}
 }
